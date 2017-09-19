89be138ec29be2b6b383017320d08d9f532cfba8
Austin <aokincaid@gmail.com>
2017-09-18 22:45:46 -0700

    Add Book DTO

---
+++ src/main/java/models/Book.java
@@ -0,0 +1,61 @@
+package models;
+
+public class Book {
+
+    private String description;
+    private String title;
+    private String price;
+    private String author;
+    private String publishDate;
+    private String genre;
+
+    public Book() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
