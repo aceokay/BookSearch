94cb97ae2d51b44fb6b086d2ff746a1de2f140fa
Austin <aokincaid@gmail.com>
2017-09-18 23:36:51 -0700

    Add two tests for basic hashing requirements

--- src/test/java/TestBookSearch.java
+++ src/test/java/TestBookSearch.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.json.simple.parser.ParseException;
@@ -10,6 +11,8 @@ import org.junit.BeforeClass;
 import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
+
+import models.Book;
 
 public class TestBookSearch {
     static private Logger LOGGER = 
@@ -30,5 +33,17 @@ public class TestBookSearch {
     @Test
     public void testLoadJson() {
         assertThat(TestBookSearch.bookSearch.getBookCount(), is(9));
+    }
+
+    @Test
+    public void testHashedBooksSingleResult() {
+        List<Book> foundBooks = TestBookSearch.bookSearch.getbooksFor("collapse");
+        assertThat(foundBooks.size(), is(1));
+    }
+
+    @Test
+    public void testHashedBooksMultipleResults() {
+        List<Book> foundBooks = TestBookSearch.bookSearch.getbooksFor("sandwich");
+        assertThat(foundBooks.size(), is(10));
     }
 }
